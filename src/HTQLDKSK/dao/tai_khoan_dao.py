20d7d30555a5a562a58e003eaac58e2c2191471d
lenguyenbangchau511 <lenguyenbangchau511@gmail.com>
2026-06-01 10:49:26 +0700

    CN0

---
+++ src/HTQLDKSK/dao/tai_khoan_dao.py
@@ -0,0 +1,62 @@
+class TaiKhoanDAO:
+
+    MA_NQ_CUSTOMER = 4
+
+    def __init__(self, db_connection):
+        self.db = db_connection
+
+    # tìm theo username
+    def get_by_username(self, username):
+        cursor = self.db.cursor(dictionary=True)
+
+        query = """
+            SELECT 
+                tk.MA_TK,
+                tk.MA_NQ,
+                tk.TEN_TK,
+                tk.MAT_KHAU,
+                tk.TRANG_THAI,
+                tk.NGAY_TAO,
+                nq.TEN_NQ
+            FROM tai_khoan tk
+            JOIN nhom_quyen nq ON tk.MA_NQ = nq.MA_NQ
+            WHERE tk.TEN_TK = %s
+        """
+
+        cursor.execute(query, (username,))
+        result = cursor.fetchone()
+        cursor.close()
+
+        return result
+
+    # tạo tài khoản
+    def create(self, username, hashed_password):
+        cursor = self.db.cursor()
+
+        query = """
+            INSERT INTO tai_khoan
+            (
+                MA_NQ,
+                TEN_TK,
+                MAT_KHAU,
+                TRANG_THAI
+            )
+            VALUES
+            (%s, %s, %s, 1)
+        """
+
+        cursor.execute(
+            query,
+            (
+                self.MA_NQ_CUSTOMER,
+                username,
+                hashed_password
+            )
+        )
+
+        self.db.commit()
+
+        ma_tk = cursor.lastrowid
+        cursor.close()
+
+        return ma_tk
